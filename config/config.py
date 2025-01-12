c89a81bd7a64686c1ad49e95f3ecacf627549912
aadit11 <aadit.kshirsagar@gmail.com>
2025-01-12 18:32:10 +0530

    Updated config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,11 @@
 config = {
-    "epochs": 10,
-    "batch_size": 32,
-    "lr": 0.001,
+    'epochs': 10,
+    'batch_size': 32,
+    'lr': 0.001,
+    'd_model': 512,
+    'n_heads': 8,
+    'd_ff': 2048,
+    'n_layers': 6,
+    'vocab_size': 5000,   
 }
+
