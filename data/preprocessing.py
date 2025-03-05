f63b11063c7dd204357d5990ebe80980f04235aa
aadit11 <aadit.kshirsagar@gmail.com>
2025-03-05 08:17:06 +0530

    changed processing

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,6 +1,6 @@
 import torch
-from collections import Counter
 from utils.tokenizer import build_vocab, tokenize
+import os
 
 def preprocess_text(input_file, output_file, vocab_size=5000, seq_length=64):
     with open(input_file, 'r') as file:
@@ -11,8 +11,10 @@ def preprocess_text(input_file, output_file, vocab_size=5000, seq_length=64):
 
     data = [tokenized_text[i:i+seq_length] for i in range(0, len(tokenized_text) - seq_length, seq_length)]
     
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    
     torch.save((data, vocab), output_file)
     print(f"Data saved to {output_file}")
 
 if __name__ == "__main__":
-    preprocess_text('data/data.txt', 'data/train_data.pt')
+    preprocess_text('data/data.txt', 'data/processed/train_data.pt')
